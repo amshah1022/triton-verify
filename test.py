544995d9a3b4f4886187b9c79cfc3e2fe3d97e88
amshah1022 <alina.shah1022@gmail.com>
2026-06-07 09:55:24 -0400

    geglu verified

--- test.py
+++ test.py
@@ -28,6 +28,8 @@ tests = [
     ("swizzle.ttir", 3000, 2, ["input_row_stride=64","scale_rows=64","scale_cols=64","grid1=2"], "BUG FOUND"),
     ("relu2.ttir", 16384, 32, ["X_stride=512", "Y_stride=512", "n_cols=512"], "SAFE"),
     ("relu2.ttir", 15000, 32, ["X_stride=512", "Y_stride=512", "n_cols=512"], "BUG FOUND"),
+    ("geglu.ttir", 16384, 32, ["stride=512", "n_cols=512"], "SAFE"),
+    ("geglu.ttir", 15000, 32, ["stride=512", "n_cols=512"], "BUG FOUND"),
 ]
 
 passed = 0
